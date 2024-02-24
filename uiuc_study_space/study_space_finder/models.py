8d690ad56d548aa9986cd9cf8a5606578d419ee8
minyoung-21 <minyoungpark5321@gmail.com>
2024-02-24 13:28:12 -0600

    database admin created

--- uiuc_study_space/study_space_finder/models.py
+++ uiuc_study_space/study_space_finder/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class BuildingDetails(models.Model):
+    building_name = models.CharField(max_length=200)
+    operating_hours = models.TimeField()
+    number_of_seats = models.IntegerField()
+    outlet_availability = models.BooleanField()
+    individual_room_availability = models.BooleanField()
