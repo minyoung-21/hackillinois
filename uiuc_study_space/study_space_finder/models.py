155f2cca51657521f2c702c59d7a52314891eea7
minyoung-21 <minyoungpark5321@gmail.com>
2024-02-24 14:55:56 -0600

    database settled

--- uiuc_study_space/study_space_finder/models.py
+++ uiuc_study_space/study_space_finder/models.py
@@ -1,9 +1,14 @@
 from django.db import models
 
+
+
 # Create your models here.
 class BuildingDetails(models.Model):
     building_name = models.CharField(max_length=200)
-    operating_hours = models.TimeField()
+    operating_start_hours = models.TimeField(editable=True, blank=True)
+    operating_end_hours = models.TimeField(editable=True, blank=True)
     number_of_seats = models.IntegerField()
     outlet_availability = models.BooleanField()
     individual_room_availability = models.BooleanField()
+    def __str__(self):
+        return self.building_name
