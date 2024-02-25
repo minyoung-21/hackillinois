7322033d3cb9ad0e8d22b04bd4995acc7370eade
Grady <grady.w.georgia@gmail.com>
2024-02-25 05:16:07 -0600

    fixed main

--- uiuc_study_space/study_space_finder/views.py
+++ uiuc_study_space/study_space_finder/views.py
@@ -1,22 +1,22 @@
 from django.shortcuts import render, get_object_or_404
 from .models import RoomDetails
-from .models import BuildingDetails
-from .utils import get_live_forecast
-from models import Building
-building_names = [building.name for building in Building.query.all()]
-set_building_names = set(building_names)
-building_data = get_live_forecast(set_building_names)
+# from .models import BuildingDetails
+# from .utils import get_live_forecast
+# from models import Building
+# building_names = [building.name for building in Building.query.all()]
+# set_building_names = set(building_names)
+# building_data = get_live_forecast(set_building_names)
 
 def list_view(request):
     return render(request, "list.html", {"room": RoomDetails.objects.all()})
 
 
-def building_detail(request, building_id):
-    building = get_object_or_404(BuildingDetails, pk=building_id)
-    forecast = get_live_forecast(building.name)
-    latitude = forecast['venue_info']['venue_address_lat']
-    longitude = forecast['venue_info']['venue_address_lng']
-    return render(request, 'building_detail.html', {'building': building, 'forecast': forecast, 'latitude': latitude, 'longitude': longitude})
+# def building_detail(request, building_id):
+#     building = get_object_or_404(BuildingDetails, pk=building_id)
+#     forecast = get_live_forecast(building.name)
+#     latitude = forecast['venue_info']['venue_address_lat']
+#     longitude = forecast['venue_info']['venue_address_lng']
+#     return render(request, 'building_detail.html', {'building': building, 'forecast': forecast, 'latitude': latitude, 'longitude': longitude})
 
 def room_details(request, room_id):
     room = get_object_or_404(RoomDetails, pk=room_id)
