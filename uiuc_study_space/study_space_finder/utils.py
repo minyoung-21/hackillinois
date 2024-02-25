26dda0e28020add9b07d2be070b60e11797d8b93
Jackcvest <jackc.vest@gmail.com>
2024-02-25 03:47:16 -0600

    building parameter stuff idk

--- uiuc_study_space/study_space_finder/utils.py
+++ uiuc_study_space/study_space_finder/utils.py
@@ -6,7 +6,7 @@ def get_live_forecasts(venue_names):
     forecasts = {}
     for venue_name in venue_names:
         params = {
-            'api_key_private': config('BESTTIME_API_KEY'),
+            'api_key_private': config('BESTTIME_PRIV_API_KEY'),
             'venue_name': venue_name,
         }
         response = requests.get(url, params=params)
