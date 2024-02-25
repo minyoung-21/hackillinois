52709fcfb1dd891a8dc15692a40fb214d22a59f7
Simon <Simonac2@illinois.edu>
2024-02-25 03:11:28 -0600

    commit at 3 AM

--- uiuc_study_space/study_space_finder/urls.py
+++ uiuc_study_space/study_space_finder/urls.py
@@ -2,8 +2,15 @@ from django.urls import path
 
 from . import views
 
+from django.urls import path
+from .views import SampleFormView
+# from django.contrib import admin
+# from . import views
+from django.urls import re_path as url
+
 urlpatterns = [
     path("", views.list_view, name="list_view"),
     path('room/<int:room_id>/', views.room_details, name='room_details'),
+    url(r'^$', SampleFormView.as_view()),
 
 ]
